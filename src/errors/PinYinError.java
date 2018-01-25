db532144c145d67ae62a855e1a651d0755c9a6b0
BeCuriousCat <1454787565@qq.com>
2018-01-25 17:03:30 +0800

    存档，网上代码被删了

--- src/errors/PinYinError.java
+++ src/errors/PinYinError.java
@@ -1,11 +1,19 @@
 package errors;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 
 import util.GeneratePY2HanZiLib;
+//pinyin4j entry point 
+import net.sourceforge.pinyin4j.PinyinHelper;
+
+//only when you need formating the output 
+import net.sourceforge.pinyin4j.format.*; 
+import net.sourceforge.pinyin4j.format.exception.*;
 
 public class PinYinError extends Error {
 
@@ -18,18 +26,95 @@ public class PinYinError extends Error {
 	}
 
 	@Override
-	public boolean process(ArrayList<StringBuffer> corpus_bf,
+	public boolean process(ArrayList<StringBuffer> corpus_bfs,
 			ArrayList<Error> errors) {
 		// TODO Auto-generated method stub
+		initPinYinLib();
 		
-		for (int i = 0; i < this.getError_size(); i++) {
-			
+		
+		//初始化pinyin4j
+		HanyuPinyinOutputFormat deFormat = new HanyuPinyinOutputFormat();
+		deFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+		deFormat.setVCharType(HanyuPinyinVCharType.WITH_V);
+		//生成错误的段落索引和序号索引
+		int paragraph_index = 0; 
+		int index = 0;
+		
+		//要改的字
+		char charAtText;
+		//是否重复
+		boolean repeat = false; 
+		StringBuffer paragraph = null;
+		//拼音
+		String[] pinyin ;
+		// 添加形近词错误
+		ArrayList<Sign> signs = new ArrayList<Sign>();
+		System.out.println("开始添加音近字错误！");
+		int count = 0; //计数器，用来选择set中的随机一个字符
+		char correct_word;
+		char wrong_word;
+		//TODO
+		for (int i = 0; i < 3; i++) {
+			//随机索引
+			paragraph_index = getRondParagraphIndex(corpus_bfs);
+			index = getRondIndex(corpus_bfs, paragraph_index);
+			while(true){
+				paragraph = corpus_bfs.get(paragraph_index);
+				charAtText = paragraph.charAt(index);
+				System.out.println("======"+charAtText);
+				try {
+					pinyin = PinyinHelper.toHanyuPinyinStringArray(charAtText,deFormat);
+					if(pinyin.length > 1){
+						System.out.println(pinyin[0]);
+					}
+					
+					if(!map.containsKey(pinyin[0])){
+						continue;
+					}else{
+						repeat = isRepeated(errors, signs, index, paragraph_index, repeat);	
+						if(repeat){
+							continue;
+						}
+						//System.out.println(map.get(pinyin[0]));
+						HashSet<Character> set = map.get(pinyin[0]);
+						int max = set.size();
+						int rand = (int) (Math.random() * max);
+						Iterator<Character> iter = set.iterator();
+						count = 0;
+						while(iter.hasNext()){
+							if(rand == count){
+								correct_word = iter.next();
+							}
+						}
+					}
+				} catch (Exception e) {
+					System.out.println(charAtText+":转换成拼音出错！");
+					e.printStackTrace();
+				}
+				
+			}
 		}
-		
 		
 		return false;
 	}
 	
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		StringBuffer sb1 = new StringBuffer();
+		StringBuffer sb2 = new StringBuffer();
+		
+		sb.append("拼音字型更新測試版，拼音字型及下載");
+		sb1.append("这几天正在教小孩学汉语拼音，本来自认为这是个简单的工作，就是重复劳动罢了");
+		sb2.append("例 tony 示例, lo拼音的咯估计很多人不知道，lo和luo 是有差别的");
+		ArrayList<StringBuffer> bfs = new ArrayList<StringBuffer>();
+		
+		bfs.add(sb);
+		bfs.add(sb1);
+		bfs.add(sb2);
+		
+		PinYinError er = new PinYinError("err", 0.01);
+		er.process(bfs, new ArrayList<Error>());
+ 	}
 	//初始化拼音查找表
 	private void initPinYinLib() {
 		GeneratePY2HanZiLib PYLib = new GeneratePY2HanZiLib();
